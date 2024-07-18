8915fc2b482ed9b958212c2cc861fcf4d3cc760f
Anthony Kim <fbssaejun@gmail.com>
2024-07-17 23:14:31 -0400

    question1 - second solution

--- Javascript/leetCode75/mergeAlternately.js
+++ Javascript/leetCode75/mergeAlternately.js
@@ -48,3 +48,22 @@ var mergeAlternately = function(word1, word2) {
   }
   return result.join('')
 };
+
+//Solution
+var mergeAlternately = function(word1, word2) {
+  let i = 0, j = 0;
+  let result = '';
+
+  while (i < word1.length || j < word2.length) {
+      if (i < word1.length) {
+          result += word1[i];
+          i++;
+      }
+      if (j < word2.length) {
+          result += word2[j];
+          j++;
+      }
+  }
+
+  return result;
+};
