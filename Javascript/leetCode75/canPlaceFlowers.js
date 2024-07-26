ecc734f61a2ee3eed033a500095877dc248a0cb6
Anthony Kim <fbssaejun@gmail.com>
2024-07-25 23:45:39 -0400

    question4 - solution 2

--- Javascript/leetCode75/canPlaceFlowers.js
+++ Javascript/leetCode75/canPlaceFlowers.js
@@ -59,3 +59,19 @@ var canPlaceFlowers = function(flowerbed, n) {
   // if available plots is bigger or equal to "n", true, else false
   return availablePlots >= n ? true : false
 };
+
+// Solution
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+  for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+    if (flowerbed[i] === 0 && flowerbed[i - 1] !== 1 && flowerbed[i + 1] !== 1) {
+      n--;
+      i++;
+    }
+  }
+  return n === 0;
+};
