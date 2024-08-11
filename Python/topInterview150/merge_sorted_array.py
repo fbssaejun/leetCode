7ba942c4ca65ef6086c491f0e54a5ae48e32c502
Anthony Kim <fbssaejun@gmail.com>
2024-08-11 16:32:09 -0400

    add test cases

--- Python/topInterview150/merge_sorted_array.py
+++ Python/topInterview150/merge_sorted_array.py
@@ -53,3 +53,8 @@ class Solution(object):
       for j in range(n):
           nums1[m+j] = nums2[j]
       nums1.sort()
+
+# Test Cases
+print(Solution().merge([1,2,3,0,0,0], 3, [2,5,6], 3))  # Output: [1,2,2,3,5,6]
+print(Solution().merge([1], 1, [], 0))  # Output: [1]
+print(Solution().merge([0], 0, [1], 1))  # Output: [1]
